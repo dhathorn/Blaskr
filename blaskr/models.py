5d59d6f2964561cee52f106cd4f15fd175d49e0f
Daniel Hathorn <dmhathorn@gmail.com>
2012-01-18 17:11:31 -0700

    added authorization to models

--- blaskr/models.py
+++ blaskr/models.py
@@ -4,6 +4,13 @@ from blaskr import db
 from flaskext.login import UserMixin
 from flask import session
 
+#helper function
+def role_number(role):
+    if type(role) == int:
+        return role
+    roles = {"Admin" : 1, "User" : 10, "Commenter" : 20}
+    return roles.get(role)
+
 #models
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,17 +55,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String())
     activate = db.Column(db.Boolean)
     created = db.Column(db.DateTime)
-    role = db.Column(db.String(15))
+    role = db.Column(db.Integer) #0 is admin, 10 is user, 20 is commentator
 
-    def __init__(self, email, password, role="User"):
+    def __init__(self, email, password, role=20):
        self.email = email
        self.password = generate_password_hash(password)
        self.activate = True #FIXME
        self.created = datetime.utcnow()
-       self.role = role
+       self.role = role_number(role)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
         return "<email %r>" % self.email
+
+    def is_authorized(self, for_role):
+        return role_number(self.role) <= role_number(for_role)
