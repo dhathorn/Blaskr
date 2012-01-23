d5852209d877090b58456d625ac6c8c17de664f0
Daniel Hathorn <dmhathorn@gmail.com>
2012-01-22 17:39:25 -0700

    removed members only views from views.py

--- blaskr/views.py
+++ blaskr/views.py
@@ -51,47 +51,16 @@ def edit():
 @app.route("/")
 def show_entries():
     entries = Post.query.order_by(Post.id.desc()).all()
-    return render_template("show_entries.html", entries=entries, form=PostForm())
+    return render_template("show_entries.html", entries=entries)
 
-@app.route("/post/<int:post_id>", methods=["GET", "POST"])
+@app.route("/post/<int:post_id>")
 def show_post(post_id):
     post = Post.query.get_or_404(post_id)
     form = PostForm(request.form)
-    if request.method == "POST":
-        if not current_user.is_authenticated():
-            return login_manager.unauthorized()
-        if form.validate():
-            populate_titletext(form, post)
-            db.session.commit()
-            flash("Successfully edited post")
-        elif form.method.data == "DELETE":
-            db.session.delete(post)
-            db.session.commit()
-            flash("Successfully deleted post")
-            return redirect(url_for("show_entries"))
-        #needs an edit post template
-
     comment = CommentForm(post_id=post_id)
     if current_user.is_authenticated():
         del comment.recaptcha
     return render_template("show_post.html", post=post, comment=comment, comments=post.comments.all())
-
-@app.route("/post/edit/<int:post_id>")
-@login_required
-def edit_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    raise notimplimentederror()
-
-@app.route("/post/add", methods=["GET", "POST"])
-@login_required
-def add_post():
-    form = PostForm(request.form)
-    if request.method == "POST" and form.validate():
-        db.session.add(Post(form.title.data, form.text.data, session["user_id"]))
-        db.session.commit()
-        flash("New entry was successfully posted")
-        return redirect(url_for("show_entries"))
-    return render_template("add_post.html", form=form)
 
 #comments
 @app.route("/comment/add", methods=["POST"])
